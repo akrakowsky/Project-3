1c73f580bbf8aaee2391459324bbfcc67bf7184e
Ali Krakowsky <alig_10@comcast.net>
2021-10-20 19:15:48 -0700

    add visual 3

--- appp.py
+++ appp.py
@@ -15,8 +15,17 @@ fig = px.pie(df, values='ID', names='Shape',
 fig.update_traces(textposition='inside', textinfo='percent+label')
 fig.show()
 
-# Create choorpleth
+# Create sunburst
 sun_df = pd.read_csv("ufo_sightings_locations.csv")
 #print(sun_df)
 sun = px.sunburst(sun_df, path=["State","City"], hover_name="City", color="ID", height=700)
 sun.show()
+
+# Create a scatter plot
+# state_df = pd.read_csv("ufo_sightings_locations.csv")
+# state_df = state_df.groupby("State")["ID"].nunique()
+# state_df.to_csv("states.csv")
+#print(state_df)
+state_df=pd.read_csv("states.csv")
+scatter = px.scatter(state_df, x="State", y="ID", height=900)
+scatter.show()
