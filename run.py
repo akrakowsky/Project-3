badaac5fc8c02ebe2f8f6e09ed703b1ea8fdd6fe
Ali Krakowsky <alig_10@comcast.net>
2021-10-22 23:36:46 -0700

    update file

--- run.py
+++ run.py
@@ -25,7 +25,6 @@ def index():
     # Create sunburst
     sun_df = pd.read_csv("ufo_sightings_locations.csv")
     #print(sun_df)
-    counts = sun_df["State"].value_counts()
     sun = px.sunburst(sun_df, title='Sunburst Plot of Locations', path=["State","City"], hover_name="City", color="City", height=700)
     #sun.show()
     fig2JSON = json.dumps(sun, cls =plotly.utils.PlotlyJSONEncoder)
